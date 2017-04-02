366b4ea64b6492e16e260906531bb88b270dc6f6
Aleksander Kwiatkowski <bobikx@poczta.fm>
2017-04-02 16:36:16 +0200

    town posts subtitle

--- data/src/views/town_view.cr
+++ data/src/views/town_view.cr
@@ -1,10 +1,35 @@
 class TownView < BaseView
   def initialize(@blog : Tremolite::Blog, @town : TownEntity)
     @url = @town.url
+
+    @posts = Array(Tremolite::Post).new
+    @blog.post_collection.each_post_from_latest do |post|
+      if @town.belongs_to_post?(post)
+        @posts << post
+      end
+    end
   end
 
   def title
     @town.name
+  end
+
+  def subtitle
+    s = @posts.size
+    return case s
+    when 0 then "brak wpisów"
+    when 1 then "1 wpis"
+    when 2, 3, 4 then "#{s} wpisy, między #{time_range_string}"
+    else "#{s} wpisów, między #{time_range_string}"
+    end
+  end
+
+  def time_range_string
+    if @posts.size > 1
+      return "#{@posts.first.date} - #{@posts.last.date}"
+    else
+      return ""
+    end
   end
 
   def image_url
@@ -20,7 +45,7 @@ class TownView < BaseView
     data = Hash(String, String).new
     data["post.image_url"] = image_url
     data["post.title"] = @town.name
-    data["post.subtitle"] = ""
+    data["post.subtitle"] = self.subtitle
     return load_html("page/header", data)
   end
 
@@ -28,14 +53,7 @@ class TownView < BaseView
     content = ""
     data = Hash(String, String).new
 
-    posts = Array(Tremolite::Post).new
-    @blog.post_collection.each_post_from_latest do |post|
-      if @town.belongs_to_post?(post)
-        posts << post
-      end
-    end
-
-    data["content"] = render_posts_preview(posts)
+    data["content"] = render_posts_preview(@posts)
     return load_html("page/article", data)
   end
 end
