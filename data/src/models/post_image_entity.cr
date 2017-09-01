6642ba46efb800b9392485c305a1d33b90c72e69
Aleksander Kwiatkowski <bobikx@poczta.fm>
2017-09-01 12:45:07 +0200

    post images urls (gallery)

--- data/src/models/post_image_entity.cr
+++ data/src/models/post_image_entity.cr
@@ -6,22 +6,23 @@ struct PostImageEntity
   THUMB_PREFIX     = "thumb"
   BIG_THUMB_PREFIX = "big_thumb"
 
-  getter :post_slug, :image, :desc
+  getter :post_slug, :post_year, :image, :desc
 
   def initialize(post, @image, @desc)
     @post_slug = post.slug
+    @post_year = post.year.to_s.as(String)
   end
 
   def full_image_src
-    "/images/#{post_slug}/#{image}"
+    "/images/#{post_year}/#{post_slug}/#{image}"
   end
 
   def thumb_image_src
-    "/images/processed/#{post_slug}_#{THUMB_PREFIX}_#{image}"
+    "/images/processed/#{post_year}/#{post_slug}_#{THUMB_PREFIX}_#{image}"
   end
 
   def big_thumb_image_src
-    "/images/processed/#{post_slug}_#{BIG_THUMB_PREFIX}_#{image}"
+    "/images/processed/#{post_year}/#{post_slug}_#{BIG_THUMB_PREFIX}_#{image}"
   end
 
   def full_image_size
