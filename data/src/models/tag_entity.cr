1152a0db547eb129794ceab35fd5709ba4d98a69
Aleksander Kwiatkowski <bobikx@poczta.fm>
2018-02-21 15:23:03 +0100

    add city tag

--- data/src/models/tag_entity.cr
+++ data/src/models/tag_entity.cr
@@ -1,18 +1,12 @@
 struct TagEntity
   @slug : String
   @name : String
-  @header_ext_img : String
-  @header_img : (String | Nil)
 
-  getter :name, :slug, :header_ext_img, :header_img
+  getter :name, :slug
 
   def initialize(y : YAML::Any)
     @slug = y["slug"].to_s
     @name = y["name"].to_s
-    @header_ext_img = y["header-ext-img"].to_s
-    if y["header_img"]?
-      @header_img = y["header_img"].to_s
-    end
   end
 
   def url
