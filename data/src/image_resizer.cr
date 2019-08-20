9f7310b634b681da4c02483e8379bbe193994ab6
Aleksander Kwiatkowski <bobikx@poczta.fm>
2019-08-20 13:18:07 +0200

    fix resize photo size for html layout

--- data/src/image_resizer.cr
+++ data/src/image_resizer.cr
@@ -1,18 +1,18 @@
 class Tremolite::ImageResizer
   @@sizez = {
     # in post content
-    "medium" => {width: 780, height: 520, quality: 88},
+    "medium" => {width: 750, height: 600, quality: 88},
 
     # in index/home page, masonry
     # M43 format for unified homepage images
-    "small" => {width: 600, height: 450, quality: 68},
+    "small" => {width: 600, height: 450, quality: 75},
     # "small"     => {width: 600, height: 400, quality: 65},
     # "small_m43" => {width: 600, height: 450, quality: 65},
 
     # in post list
-    "thumb" => {width: 60, height: 40, quality: 50},
+    "thumb" => {width: 60, height: 40, quality: 70},
     # in map probably
-    "big_thumb" => {width: 150, height: 100, quality: 60},
+    "big_thumb" => {width: 150, height: 100, quality: 70},
   }
   @@quality = 70
 end
