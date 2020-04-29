a4717fd12d887755a273bc5eba3a53236686fa34
Aleksander Kwiatkowski <bobikx@poczta.fm>
2020-04-29 18:02:17 +0200

    unify watch+compile script for local (my) env

--- watch.sh
+++
@@ -1,6 +0,0 @@
-inotifywait -e close_write,moved_to,create -mr data/ src/ src/ |
-while read path action file; do
-  echo "The file '$file' appeared in directory '$path' via '$action'"
-  crystal src/odkrywajac_polske.cr
-  echo "Done"
-done
--- watch_smart.sh
+++ watch_smart.sh
@@ -1,18 +1,26 @@
-inotifywait -e close_write,moved_to,create -mr data/ /home/olek/projects/self/crystal/tremolite/src/ src/ |
-while read path action file; do
+# refactored while loop because I had problem with files changes during
+# blog rendering
+while :
+do
+  inotify_output=`inotifywait -e close_write,moved_to,create -r data/ /home/olek/projects/self/crystal/tremolite/src/ src/ --exclude public/`
+  # assing vars
+  read -r path action file <<< "$inotify_output"
+
+  echo "file '$file' in '$path' via '$action'"
+
   if [ ${file: -3} == ".cr" ]
   then
-    echo "The file '$file' appeared in directory '$path' via '$action'"
-
     # --release flag is not suitable here
-    rm ./blog
-    crystal src/odkrywajac_polske.cr -o blog
-    echo "Compiled"
 
-    ./blog
+    rm ./blog
+    crystal src/odkrywajac_polske_local.cr -o blog #--error-trace
+
+    echo "compiled"
   else
-    echo "The file '$file' appeared in directory '$path' via '$action'"
-    ./blog
+    echo "running"
   fi
+
+  CRYSTAL_LOG_LEVEL=DEBUG CRYSTAL_LOG_SOURCES="*" ./blog
+
   echo "Done"
 done
