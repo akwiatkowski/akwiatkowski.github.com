ae0cf52df016c7bcf6a39b3268094d01151a420f
Aleksander Kwiatkowski <bobikx@poczta.fm>
2020-10-08 18:04:18 +0200

    burnout stats refactoring

--- data/src/services/burnout_stat.cr
+++ data/src/services/burnout_stat.cr
@@ -8,10 +8,19 @@ class BurnoutStat
     distance_change: Int32?,
     distance_change_percent: Int32?,
 
+    distance_avg: Int32?,
+    distance_avg_change: Int32?,
+    distance_avg_change_percent: Int32?,
+
     time_spent: Int32,
     time_spent_last_year: Int32?,
     time_spent_change: Int32?,
     time_spent_change_percent: Int32?,
+
+    time_spent_avg: Int32?,
+    time_spent_avg_change: Int32?,
+    time_spent_avg_change_percent: Int32?,
+
   )
   alias MonthStatTuple = NamedTuple(
     distance: Int32,
@@ -71,10 +80,14 @@ class BurnoutStat
       distance_change_percent = nil
 
       time_spent_last_year= nil
+      time_spent_change = nil
+      time_spent_change_percent = nil
+
+      distance_current_month = month_data[month][:distance]
+      time_spent_current_month = month_data[month][:time_spent]
 
       if month_year_before
         distance_year_before = month_year_before[:distance]
-        distance_current_month = month_data[month][:distance]
         if distance_year_before > 0 && distance_current_month > 0
           distance_last_year = distance_year_before
           distance_change = distance_current_month - distance_year_before
@@ -82,11 +95,43 @@ class BurnoutStat
         end
 
         time_spent_year_before = month_year_before[:time_spent]
-        time_spent_current_month = month_data[month][:time_spent]
         if time_spent_year_before > 0 && time_spent_current_month > 0
           time_spent_last_year = time_spent_year_before
           time_spent_change = time_spent_current_month - time_spent_year_before
           time_spent_change_percent = ((time_spent_change.to_f / time_spent_year_before.to_f) * 100.0).to_i
+        end
+      end
+
+      # calculate average for all previous years
+      distance_avg = nil
+      distance_avg_change = nil
+      distance_avg_change_percent = nil
+
+      time_spent_avg = nil
+      time_spent_avg_change = nil
+      time_spent_avg_change_percent = nil
+
+      month_keys_for_avg = month_data.keys.select do |m|
+        # `month` is Time variable
+        m.month == month.month && m.year < month.year
+      end
+
+      puts month_keys_for_avg.inspect
+
+      if month_keys_for_avg.size > 0
+        # calculate average
+        avg_size = month_keys_for_avg.size
+        distance_avg = (month_keys_for_avg.map {|m| month_data[m][:distance] }.sum.to_f / avg_size.to_f).to_i
+        time_spent_avg = (month_keys_for_avg.map {|m| month_data[m][:time_spent] }.sum.to_f / avg_size.to_f).to_i
+
+        if distance_avg > 0 && distance_current_month > 0
+          distance_avg_change = distance_current_month - distance_avg
+          distance_avg_change_percent = ((distance_avg_change.to_f / distance_avg.to_f) * 100.0).to_i
+        end
+
+        if time_spent_avg > 0 && time_spent_current_month > 0
+          time_spent_avg_change = time_spent_current_month - time_spent_avg
+          time_spent_avg_change_percent = ((time_spent_avg_change.to_f / time_spent_avg.to_f) * 100.0).to_i
         end
       end
 
@@ -98,10 +143,18 @@ class BurnoutStat
         distance_change: distance_change,
         distance_change_percent: distance_change_percent,
 
+        distance_avg: distance_avg,
+        distance_avg_change: distance_avg_change,
+        distance_avg_change_percent: distance_avg_change_percent,
+
         time_spent: month_data[month][:time_spent],
         time_spent_last_year: time_spent_last_year,
         time_spent_change: time_spent_change,
         time_spent_change_percent: time_spent_change_percent,
+
+        time_spent_avg: time_spent_avg,
+        time_spent_avg_change: time_spent_avg_change,
+        time_spent_avg_change_percent: time_spent_avg_change_percent,
       )
 
       # TODO add to Time struct
--- data/src/views/burnout_stat_view.cr
+++ data/src/views/burnout_stat_view.cr
@@ -22,6 +22,48 @@ class BurnoutStatView < WidePageView
     return "background-color: rgba(#{color_string},#{opacity});"
   end
 
+  # def render_cell(
+  #   record_value = nil,
+  #   positive_max = 100,
+  #   positive_color_string = "0,255,0", # "0,192,255"
+  #   negative_max = -100,
+  #   negative_color_string = "255,0,0",
+  #   unit_name = ""
+
+  def render_cell(
+    record_value = nil,
+    positive_max = 100,
+    positive_color_string = "0,255,0", # "0,192,255"
+    negative_max = -100,
+    negative_color_string = "255,0,0",
+    unit_name = ""
+  )
+    opacity_style = ""
+    if record_value
+      if record_value.not_nil! > 0
+        opacity_style = opacity_style_string(
+          value: record_value,
+          max: positive_max,
+          color_string: positive_color_string,
+        )
+      else
+        opacity_style = opacity_style_string(
+          value: record_value,
+          max: negative_max,
+          color_string: negative_color_string,
+        )
+      end
+    end
+
+    return String.build do |str|
+      str << "<td style='#{opacity_style}' >"
+      if record_value
+        str << "#{record_value}#{unit_name}"
+      end
+      str << "</td>\n"
+    end
+  end
+
   def inner_html
     data = @stat.make_it_so
 
@@ -38,168 +80,122 @@ class BurnoutStatView < WidePageView
       str << "<th>Miesiąc</th>\n"
 
       str << "<th>Poprz</th>\n"
+      str << "<th>Śr.</th>\n"
       str << "<th>Akt.</th>\n"
       str << "<th>Zmiana</th>\n"
-      str << "<th>%</th>\n"
 
       str << "<th>Poprz</th>\n"
+      str << "<th>Śr.</th>\n"
       str << "<th>Akt.</th>\n"
       str << "<th>Zmiana</th>\n"
-      str << "<th>%</th>\n"
 
       str << "</tr>\n"
 
-
-      data.each do |record|
+      data.reverse.each do |record|
         str << "<tr>\n"
 
         str << "<td>#{record[:month].to_s("%Y-%m")}</td>\n"
 
         # distance, last year
-        opacity_style = opacity_style_string(
-          value: record[:distance_last_year],
-          max: 400,
-          color_string: "70,255,70"
+        str << render_cell(
+          record_value: record[:distance_last_year],
+          positive_max: 400,
+          positive_color_string: "70,255,70",
+          negative_max: -100,
+          negative_color_string: "255,255,255",
+          unit_name: "km",
         )
-        str << "<td style='#{opacity_style}' >"
-        if record[:distance_last_year]
-          str << "#{record[:distance_last_year]}km"
-        end
-        str << "</td>\n"
+
+        # distance, avg
+        str << render_cell(
+          record_value: record[:distance_avg],
+          positive_max: 400,
+          positive_color_string: "70,255,70",
+          negative_max: -100,
+          negative_color_string: "255,255,255",
+          unit_name: "km",
+        )
 
         # distance, current year
-        opacity_style = opacity_style_string(
-          value: record[:distance],
-          max: 400,
-          color_string: "0,255,0"
+        str << render_cell(
+          record_value: record[:distance],
+          positive_max: 400,
+          positive_color_string: "0,255,0",
+          negative_max: -100,
+          negative_color_string: "255,255,255",
+          unit_name: "km",
         )
-        str << "<td style='#{opacity_style}' >"
-        if record[:distance]
-          str << "#{record[:distance]}km"
-        end
-        str << "</td>\n"
 
         # distance, delta
-        opacity_style = ""
-        if record[:distance_change]
-          if record[:distance_change].not_nil! > 0
-            opacity_style = opacity_style_string(
-              value: record[:distance_change],
-              max: 400,
-              color_string: "0,128,255"
-            )
-          else
-            opacity_style = opacity_style_string(
-              value: record[:distance_change],
-              max: -400,
-              color_string: "255,0,0"
-            )
-          end
-        end
+        str << render_cell(
+          record_value: record[:distance_avg_change],
+          positive_max: 400,
+          positive_color_string: "0,128,255",
+          negative_max: -400,
+          negative_color_string: "255,0,0",
+          unit_name: "km",
+        )
 
-        str << "<td style='#{opacity_style}' >"
-        if record[:distance_change]
-          str << "#{record[:distance_change]}km"
-        end
-        str << "</td>\n"
-
-        # distance delta, percentage
-        opacity_style = ""
-        if record[:distance_change_percent]
-          if record[:distance_change_percent].not_nil! > 0
-            opacity_style = opacity_style_string(
-              value: record[:distance_change_percent],
-              max: 100,
-              color_string: "0,128,255"
-            )
-          else
-            opacity_style = opacity_style_string(
-              value: record[:distance_change_percent],
-              max: -100,
-              color_string: "255,0,0"
-            )
-          end
-        end
-
-        str << "<td style='#{opacity_style}' >"
-        if record[:distance_change_percent]
-          str << "#{record[:distance_change_percent]}%"
-        end
-        str << "</td>\n"
-
+        # # distance delta, percentage
+        # str << render_cell(
+        #   record_value: record[:distance_change_percent],
+        #   positive_max: 100,
+        #   positive_color_string: "0,128,255",
+        #   negative_max: -100,
+        #   negative_color_string: "255,0,0",
+        #   unit_name: "%",
+        # )
 
         # time_spent, last year
-        opacity_style = opacity_style_string(
-          value: record[:time_spent_last_year],
-          max: 50,
-          color_string: "255,255,0"
+        str << render_cell(
+          record_value: record[:time_spent_last_year],
+          positive_max: 50,
+          positive_color_string: "255,255,0",
+          negative_max: -50,
+          negative_color_string: "255,255,255",
+          unit_name: "h",
         )
-        str << "<td style='#{opacity_style}' >"
-        if record[:time_spent_last_year]
-          str << "#{record[:time_spent_last_year]}h"
-        end
-        str << "</td>\n"
 
-        # distance, current year
-        opacity_style = opacity_style_string(
-          value: record[:time_spent],
-          max: 50,
-          color_string: "255,255,0"
+        # time_spent, average
+        str << render_cell(
+          record_value: record[:time_spent_avg],
+          positive_max: 50,
+          positive_color_string: "255,255,0",
+          negative_max: -50,
+          negative_color_string: "255,255,255",
+          unit_name: "h",
         )
-        str << "<td style='#{opacity_style}' >"
-        if record[:time_spent]
-          str << "#{record[:time_spent]}h"
-        end
-        str << "</td>\n"
 
+        # time_spent, current year
+        str << render_cell(
+          record_value: record[:time_spent],
+          positive_max: 50,
+          positive_color_string: "255,255,0",
+          negative_max: -50,
+          negative_color_string: "255,255,255",
+          unit_name: "h",
+        )
 
         # time_spent, delta
-        opacity_style = ""
-        if record[:time_spent_change]
-          if record[:time_spent_change].not_nil! > 0
-            opacity_style = opacity_style_string(
-              value: record[:time_spent_change],
-              max: 40,
-              color_string: "0,128,255"
-            )
-          else
-            opacity_style = opacity_style_string(
-              value: record[:time_spent_change],
-              max: -40,
-              color_string: "255,0,0"
-            )
-          end
-        end
+        str << render_cell(
+          record_value: record[:time_spent_avg_change],
+          positive_max: 40,
+          positive_color_string: "0,128,255",
+          negative_max: -40,
+          negative_color_string: "255,0,0",
+          unit_name: "h",
+        )
 
-        str << "<td style='#{opacity_style}' >"
-        if record[:time_spent_change]
-          str << "#{record[:time_spent_change]}km"
-        end
-        str << "</td>\n"
-
-        # distance delta, percentage
-        opacity_style = ""
-        if record[:time_spent_change_percent]
-          if record[:time_spent_change_percent].not_nil! > 0
-            opacity_style = opacity_style_string(
-              value: record[:time_spent_change_percent],
-              max: 100,
-              color_string: "0,128,255"
-            )
-          else
-            opacity_style = opacity_style_string(
-              value: record[:time_spent_change_percent],
-              max: -100,
-              color_string: "255,0,0"
-            )
-          end
-        end
-
-        str << "<td style='#{opacity_style}' >"
-        if record[:time_spent_change_percent]
-          str << "#{record[:time_spent_change_percent]}%"
-        end
-        str << "</td>\n"
+        # # distance delta, percentage
+        # opacity_style = ""
+        # str << render_cell(
+        #   record_value: record[:time_spent_change_percent],
+        #   positive_max: 100,
+        #   positive_color_string: "0,128,255",
+        #   negative_max: -100,
+        #   negative_color_string: "255,0,0",
+        #   unit_name: "%",
+        # )
 
         # end of row
         str << "</tr>\n"
