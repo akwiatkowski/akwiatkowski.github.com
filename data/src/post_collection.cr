586cbc849117579defeb667b8638a76f9b135cc6
Aleksander Kwiatkowski <bobikx@poczta.fm>
2026-01-31 07:12:12 +0100

    load posts from subdirectories also

---
+++ data/src/post_collection.cr
@@ -0,0 +1,7 @@
+class Tremolite::PostCollection
+  def each_post_file(&block : String -> Nil)
+    Dir[File.join([@posts_path, "**", "*.#{@posts_ext}"])].sort.each do |post_path|
+      block.call(post_path)
+    end
+  end
+end
